4c3365c79abf85f804daff8fec0098459a429b57
sathachie <sathachie@gmail.com>
2013-05-28 15:17:09 +0900

    3rd commit

---
+++ testGitHub2/SHEventDataController.h
@@ -0,0 +1,13 @@
+//
+//  SHEventDataController.h
+//  testGitHub2
+//
+//  Created by sat on 2013/05/28.
+//  Copyright (c) 2013年 SH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SHEventDataController : NSObject
+
+@end
