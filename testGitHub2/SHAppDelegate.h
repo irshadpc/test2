cbdacadb5ee556f0eda13d327faec4fb19908c6d
sathachie <sathachie@gmail.com>
2013-05-28 11:17:04 +0900

    Initial Commit

---
+++ testGitHub2/SHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SHAppDelegate.h
+//  testGitHub2
+//
+//  Created by sat on 2013/05/28.
+//  Copyright (c) 2013年 SH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ testGitHub2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  testGitHub2
+//
+//  Created by sat on 2013/05/28.
+//  Copyright (c) 2013年 SH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SHAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SHAppDelegate class]));
+    }
+}
